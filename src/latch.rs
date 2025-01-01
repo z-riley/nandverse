e522a52bd71b3ee5ab1dc04b87706231c2800063
Zac <zac460@hotmail.co.uk>
2025-01-01 09:50:51 +0000

    Add D type latch

--- src/latch.rs
+++ src/latch.rs
@@ -7,10 +7,11 @@ pub struct SRLatch {
 
 impl SRLatch {
     /// Creates a new SR latch in the reset state.
-    pub fn new() -> SRLatch {
+    pub fn new() -> Self {
         SRLatch { q: false, qn: true }
     }
 
+    /// Set the set and reset inputs
     pub fn set(&mut self, s: bool, r: bool) {
         if s && r {
             panic!("restricted combination");
@@ -52,6 +53,41 @@ impl Default for SRLatch {
     }
 }
 
+pub struct DLatch {
+    sr_latch: SRLatch,
+}
+
+impl DLatch {
+    /// Creates a new D latch in the reset state.
+    pub fn new() -> Self {
+        DLatch {
+            sr_latch: SRLatch::new(),
+        }
+    }
+
+    /// Set the enable and data inputs
+    pub fn set(&mut self, e: bool, d: bool) {
+        let dn = gate::not(&d);
+        let s = gate::and(&[d, e]);
+        let r = gate::and(&[dn, e]);
+        self.sr_latch.set(s, r);
+    }
+
+    pub fn q(&self) -> bool {
+        self.sr_latch.q()
+    }
+
+    pub fn qn(&self) -> bool {
+        self.sr_latch.qn()
+    }
+}
+
+impl Default for DLatch {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -75,12 +111,40 @@ mod tests {
                 qn: !q_init,
             };
 
-            dbg!(q_init, s, r);
-
             // Send test signals
             latch.set(s, r);
 
             assert_eq!(latch.q(), q_expected, "failed for inputs: {:?}", (s, r))
+        }
+    }
+
+    #[test]
+    fn test_d_latch() {
+        for (q_init, e, d, q_expected) in [
+            // Hold state
+            (false, false, false, false),
+            (true, false, false, true),
+            (false, false, true, false),
+            (true, false, true, true),
+            // Reset
+            (false, true, false, false),
+            (true, true, false, false),
+            // Set
+            (false, true, true, true),
+            (true, true, true, true),
+        ] {
+            // Set up initial state
+            let mut latch = DLatch {
+                sr_latch: SRLatch {
+                    q: q_init,
+                    qn: !q_init,
+                },
+            };
+
+            // Send test signals
+            latch.set(e, d);
+
+            assert_eq!(latch.q(), q_expected, "failed for inputs: {:?}", (e, d))
         }
     }
 }
