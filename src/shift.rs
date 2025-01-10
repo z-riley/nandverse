6f3a779d1eb1b335b606ec48aacbe8dbc1fce7ca
Zac <zac460@hotmail.co.uk>
2025-01-10 19:36:16 +0000

    Condense shifter

--- src/shift.rs
+++ src/shift.rs
@@ -1,51 +1,19 @@
 use crate::mux;
 
 pub fn logical_shift_right_8(shift: &[bool; 3], value: &[bool; 8]) -> [bool; 8] {
-    [
-        mux::mux8(shift, value),
-        mux::mux8(
-            shift,
-            &[
-                value[1], value[2], value[3], value[4], value[5], value[6], value[7], value[0],
-            ],
-        ),
-        mux::mux8(
-            shift,
-            &[
-                value[2], value[3], value[4], value[5], value[6], value[7], value[0], value[1],
-            ],
-        ),
-        mux::mux8(
-            shift,
-            &[
-                value[3], value[4], value[5], value[6], value[7], value[0], value[1], value[2],
-            ],
-        ),
-        mux::mux8(
-            shift,
-            &[
-                value[4], value[5], value[6], value[7], value[0], value[1], value[2], value[3],
-            ],
-        ),
-        mux::mux8(
-            shift,
-            &[
-                value[5], value[6], value[7], value[0], value[1], value[1], value[2], value[3],
-            ],
-        ),
-        mux::mux8(
-            shift,
-            &[
-                value[6], value[7], value[0], value[1], value[2], value[3], value[4], value[5],
-            ],
-        ),
-        mux::mux8(
-            shift,
-            &[
-                value[7], value[0], value[1], value[2], value[3], value[4], value[5], value[6],
-            ],
-        ),
-    ]
+    let mut output = [false; 8];
+    for i in 0..8 {
+        dbg!(i);
+        let mut input = [false; 8];
+        for j in 0..8 {
+            let n = (i + j) % 8;
+            input[j] = value[n];
+        }
+        output[i] = mux::mux8(shift, &input);
+        dbg!(input);
+    }
+
+    output
 }
 
 #[cfg(test)]
