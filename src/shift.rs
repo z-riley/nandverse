b69930c99f260a7f50176315b6bdaaf786a2282b
Zac <zac460@hotmail.co.uk>
2025-01-10 19:37:08 +0000

    Use foreach intead of for

--- src/shift.rs
+++ src/shift.rs
@@ -2,16 +2,16 @@ use crate::mux;
 
 pub fn logical_shift_right_8(shift: &[bool; 3], value: &[bool; 8]) -> [bool; 8] {
     let mut output = [false; 8];
-    for i in 0..8 {
+    (0..8).for_each(|i| {
         dbg!(i);
         let mut input = [false; 8];
-        for j in 0..8 {
+        (0..8).for_each(|j| {
             let n = (i + j) % 8;
             input[j] = value[n];
-        }
+        });
         output[i] = mux::mux8(shift, &input);
         dbg!(input);
-    }
+    });
 
     output
 }
